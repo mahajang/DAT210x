14689b03ca6f06e71ae1a64295709c6cda4d1f65
mahajang <gaurav.mahajan@hpe.com>
2016-11-03 00:51:28 +0530

    Update assignment2.py

--- Module2/assignment2.py
+++ Module2/assignment2.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 # TODO: Load up the 'tutorial.csv' dataset
 #
-# .. your code here ..
+df=pd.read_csv('D:/Mindtree_work_16052016/Python/Python_for_Programming_edX/Module2_Datasets/tutorial.csv')
 
 
 
 # TODO: Print the results of the .describe() method
 #
-# .. your code here ..
+print df.describe()
 
 
 
@@ -16,5 +16,5 @@ import pandas as pd
 # use in order to index your dataframe with: [2:4,'col3']
 # And print the results
 #
-# .. your code here ..
+df.loc[2:4,['col3']]
 
